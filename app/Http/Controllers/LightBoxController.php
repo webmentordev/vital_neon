131cffb59311a474013185220045b530448337e5
Muhammad Ahmer Tahir <82140155+webmentordev@users.noreply.github.com>
2024-04-24 17:55:27 +0500

    Update all/status/feature

--- app/Http/Controllers/LightBoxController.php
+++ app/Http/Controllers/LightBoxController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\LightBox;
 use Stripe\StripeClient;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class LightBoxController extends Controller
 {
@@ -68,5 +69,55 @@ class LightBoxController extends Controller
             'price' => $request->price
         ]);
         return back()->with('success', 'LightBox has been added!');
-    }    
+    }
+    public function update_box(Request $request, LightBox $light_box){
+        $stripe = new StripeClient(config('app.stripe'));
+        $this->validate($request, [
+            'title' => "required|max:255",
+            'light_image' => "nullable|image|mimes:png,jpg,jpeg,webp",
+            'dark_image' => "nullable|image|mimes:png,jpg,jpeg,webp",
+            'slug' => "required|max:255",
+            'price' => "required|numeric|min:1",
+            'body' => "required",
+            'description' => "required"
+        ]);
+        $slug = strtolower(str_replace(' ', '-', $request->slug));
+        $light_image = null;
+        $dark_image = null;
+        if($request->hasFile('light_image')){
+            Storage::disk('public_disk')->delete($light_box->light_image);
+            $light_image = $request->light_image->storeAs('lightboxes', $slug.'-light-'.rand(9,999999).".".$request->light_image->getClientOriginalExtension(), 'public_disk');
+            $stripe->products->update(
+                $light_box->stripe_id,
+                [
+                    'images' => [
+                        config('app.url').'/storage/'.$light_image
+                    ]
+                ]
+            );
+        }
+        if($request->hasFile('dark_image')){
+            if($light_box->dark_image){
+                Storage::disk('public_disk')->delete($light_box->dark_image);
+            }
+            $dark_image = $request->dark_image->storeAs('lightboxes', $slug.'-dark-'.rand(9,999999).".".$request->dark_image->getClientOriginalExtension(), 'public_disk');
+        }
+
+        if($request->title != $light_box->title){
+            $stripe->products->update(
+                $light_box->stripe_id,
+                ['name' => $request->title]
+            );
+        }
+        $light_box->update(array_filter([
+            'title' => $request->title,
+            'slug' => $slug,
+            'price' => $request->price,
+            'light_image' => $light_image,
+            'dark_image' => $dark_image,
+            'body' => $request->body,
+            'description' => $request->description
+        ]));
+        return back()->with('success', 'LightBox has been updated');
+    }
 }
