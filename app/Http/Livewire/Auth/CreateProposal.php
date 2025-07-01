4c5ceab319a1696a1eae9412ed0a764a8a126524
webmentordev <ahmertahir99@gmail.com>
2025-07-01 20:32:26 +0500

    Create Options

--- app/Http/Livewire/Auth/CreateProposal.php
+++ app/Http/Livewire/Auth/CreateProposal.php
@@ -26,17 +26,17 @@ class CreateProposal extends Component
             'mockup' => null,
             'sizes'=> [
                 [ 
-                    "size" => "Small",
+                    "size" => "Original",
                     "price"=> "", 
                     "dimensions"=> "",  
                 ],
                 [ 
-                    "size" => "Medium",
+                    "size" => "Option-1",
                     "price"=> "", 
                     "dimensions"=> "",  
                 ],
                 [ 
-                    "size" => "Large",
+                    "size" => "Option-2",
                     "price"=> "", 
                     "dimensions"=> "",  
                 ]
